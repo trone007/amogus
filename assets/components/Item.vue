93849641de02bc551df90fa3241f3f7d48b04961
pindzak <z273-i@yandex.ru>
2021-07-03 15:32:41 +0200

    vue

---
+++ assets/components/Item.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		asd1
+<!--		<span v-bind:class="{done: item.completed}">-->
+<!--			<input type="checkbox" v-on:change="item.completed = !item.completed">-->
+<!--			<strong>{{ index + 1 }}</strong>-->
+<!--			{{ uppercase }}-->
+<!--		</span>-->
+<!--		<button v-on:click="$emit('item-remove', item.id)">-->
+<!--			&times;-->
+<!--		</button>-->
+	</div>
+</template>
