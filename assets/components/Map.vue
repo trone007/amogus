757e33ff0276c0c7ca4dbcceb322cdc7d8fc1827
pindzak <z273-i@yandex.ru>
2021-07-03 18:46:30 +0200

    map map

--- assets/components/Map.vue
+++ assets/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div id="map" style="width: 600px; height: 400px">
+		<div id="map" style="width: 100%; height: 1000px">
 		</div>
 	</div>
 </template>
@@ -13,6 +13,7 @@ export default {
 		return {
 			map: null,
 			connection: null,
+			userId: null,
 			persons: [],
 		}
 	},
@@ -30,13 +31,13 @@ export default {
 		// 	console.log("CONNECTED");
 		// 	console.log("open");
 		// });
-
-
 	},
 
 	methods: {
 		initMap()
 		{
+			this.userId = new URL(document.location).searchParams.get('userId');
+
 			this.map = new ymaps.Map("map", {
 				center: [54.660901, 20.932067],
 				zoom: 19,
@@ -50,22 +51,25 @@ export default {
 			location.then(
 				result =>
 				{
-					console.log(result.geoObjects.position);
 					this.map.setCenter(result.geoObjects.position, 19);
 
+					// const me = new ymaps.Placemark(result.geoObjects.position, {}, {
+					// 	iconLayout: 'default#image',
+					// 	iconImageHref: '/img/me.png',
+					// 	iconImageSize: [30, 30],
+					// 	iconImageOffset: [-15, -15]
+					// });
+
 					const me = new ymaps.Placemark(result.geoObjects.position, {}, {
-						iconLayout: 'default#image',
-						iconImageHref: '/test/img/me.png',
-						iconImageSize: [30, 30],
-						iconImageOffset: [-15, -15]
+						preset: 'islands#redIcon'
 					});
 					const fov = new ymaps.Circle([result.geoObjects.position, 20]);
 					this.map.geoObjects.add(me);
 					this.map.geoObjects.add(fov);
 
 					const message = {
-						userId: 1,
-						login: 'login',
+						userId: this.userId,
+						// login: 'login',
 						position: result.geoObjects.position,
 					};
 					this.connection.send(JSON.stringify(message));
@@ -74,6 +78,11 @@ export default {
 					{
 						const message = e.data;
 						console.log(message);
+
+						if(message.userId !== this.userId)
+						{
+							// this.addPerson(message);
+						}
 					});
 				},
 				err =>
@@ -86,11 +95,15 @@ export default {
 		addPerson(data)
 		{
 			this.persons.push(data);
-			const pers = new ymaps.Placemark(data.position, {}, {
-				iconLayout: 'default#image',
-				iconImageHref: '/test/img/me.png',
-				iconImageSize: [30, 30],
-				iconImageOffset: [-35, -55]
+			// const pers = new ymaps.Placemark(data.position, {}, {
+			// 	iconLayout: 'default#image',
+			// 	iconImageHref: '/img/pers.png',
+			// 	iconImageSize: [30, 30],
+			// 	iconImageOffset: [-15, -15]
+			// });
+
+			const pers = new ymaps.Placemark(result.geoObjects.position, {}, {
+				preset: 'islands#redIcon'
 			});
 			this.map.geoObjects.add(pers);
 		}
